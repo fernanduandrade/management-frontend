526156d0329e53372e6d3459e8d64b7713cab19b
fernanduandrade <onanduandrade@gmail.com>
2024-02-14 21:42:40 -0300

    fix: adicionado campo com total vendido do dia

--- src/api/Sale/SaleApi.ts
+++ src/api/Sale/SaleApi.ts
@@ -21,6 +21,13 @@ class SaleApi implements ISaleApi {
     return result
   }
 
+  async todaySales(): Promise<IResponseDTO<number>> {
+    const url = 'http://localhost:5019/api/v1/saleshistory/today'
+    const reponse = await apiService.get<IResponseDTO<number>>(url)
+    const result = reponse.get()
+    return result
+  }
+
   updateSale(): Promise<void> {
     throw new Error('Method not implemented.')
   }
--- src/sales/views/sales/index.vue
+++ src/sales/views/sales/index.vue
@@ -5,16 +5,21 @@ import { SaleDTO } from '~/sales/types/index'
 import { useFilter } from '~/composables/index'
 import useModal from '~/common/logic/use-modal'
 import SaleForm from '~/sales/components/SaleForm.vue'
+import { formatCurrency } from '~/common/logic'
 const saleColumn = ref<string[]>([
   'clientName', 'totalPrice', 'pricePerUnit', 'saleDate', 'quantity',
 ])
 
 const sales = ref<SaleDTO[]>([])
+const todaySales = ref(0)
 const { search, data } = useFilter(sales, 'clientName')
 
 onMounted(async() => {
   const result = await SaleApi.getSalesPaginate({ pageNumber: 1, pageSize: 10 })
   sales.value = result.data.items
+
+  const salesOfToday = await SaleApi.todaySales()
+  todaySales.value = salesOfToday.data
 })
 
 const storeModal = useModal()
@@ -33,10 +38,15 @@ function createSaleModal() {
 <template>
   <main class="container">
     <div class="sale__actions">
-      <VInputSearch v-model="search" placeholder="Pesquise pelo nome do cliente" />
-      <VButton @click="createSaleModal">
-        Nova venda
-      </VButton>
+      <h2 class="sale__made">
+        Vendas do dia {{ formatCurrency(todaySales) }}
+      </h2>
+      <div class="sale__inputs">
+        <VInputSearch v-model="search" placeholder="Pesquise pelo nome do cliente" />
+        <VButton @click="createSaleModal">
+          Nova venda
+        </VButton>
+      </div>
     </div>
     <VTable
       :columns="saleColumn"
@@ -56,7 +66,18 @@ function createSaleModal() {
 }
 .sale__actions {
   display: flex;
-  justify-content: flex-end;
+  justify-content: space-between;
+  align-items: center;
   gap: 10px;
+}
+
+.sale__inputs {
+  display: flex;
+  gap: 20px;
+}
+
+.sale__made {
+  font-weight: 500;
+  font-size: 20px;
 }
 </style>
